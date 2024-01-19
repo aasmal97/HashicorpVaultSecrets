3288ad12d8d91b5931932cc68bb20dfaf57cbb8a
Arky Asmal <arkyasmal@gmail.com>
2024-01-18 19:50:27 -0500

    add new dist to repo

--- dist/action/index.js
+++ dist/action/index.js
@@ -18915,7 +18915,8 @@ var installHashiCorp = async () => {
     await execShellCommand(
       "echo 'deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/hashicorp.list"
     );
-    await execShellCommand("sudo apt update && sudo apt install vlt -y");
+    await execShellCommand("sudo apt update");
+    await execShellCommand("sudo apt install vlt -y");
     await execShellCommand("vlt");
     core.info("HashiCorp Vault Installed");
   } catch (error2) {
